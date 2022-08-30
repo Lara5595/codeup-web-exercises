a2a65179f50b0824863deccf5ebb19b32f9f402b
David Lara <david.a.lara05@gmail.com>
2022-08-30 09:34:04 -0500

    external stylesheet

--- js/warmups.js
+++ js/warmups.js
@@ -191,4 +191,33 @@ function beeramid(bonus,price) {
         totalCost += levels * levels * price;
         levels++;
     }
+    return levels;
 }
+
+
+
+
+
+// Write a function, getSimpleUser, that takes in a user object and returns a simplified user object.
+//
+//     Assume the following shape of the user object for the function input:
+//
+//     ```
+// {
+//   firstName: '...',
+//   lastName: '...',
+//   email: '...',
+//   username: '...',
+//   password: '...'
+// }
+// ```
+//
+// The shape of the output should be the following:
+//
+//     ```
+// {
+//   firstName: '...',
+//   lastName: '...',
+// }
+//
+// Regardless of the case of the first name and last name input, the output should be in all lower case.
