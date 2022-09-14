7c3f2b04572023f05905f4c9bb24dd37a2a7d754
David Lara <david.a.lara05@gmail.com>
2022-09-14 14:15:53 -0500

    created defuse the bomb

--- js/browser-object-model-lecture.js
+++ js/browser-object-model-lecture.js
@@ -25,4 +25,42 @@ function hey() {
    thisTimeItWillStop = setInterval(hey, 1000)
 }
 
+//          or
+
+// let sayHey = function () {
+//     let count = 0;
+//     function hey() {
+//         if (count < 10) {
+//             console.log('hey ' + count);
+//         } else {
+//             clearInterval(setInterval((hey, 1000)))
+//         }
+//     }
+// }
 // This function ^^ will make it stop even in the background
+
+//              or
+
+// function tenHeys() {
+//     let count = 0;
+//     let thisWillStop = setInterval(function (){
+//         if (count < 10) {
+//             console.log('hey ' + count);
+//             count++
+//         } else {
+//             clearInterval(thisWillStop)
+//         }
+//     }, 1000);
+// }
+
+
+
+
+// setTimeout is a timer
+setTimeout(wakeUpUser, 5000); //This takes the function and alerts wakee upp after 5sec
+
+function wakeUpUser() {
+    alert("WAKE UUUUPPPP!!!!");   // function for setTimeout
+}
+
+
