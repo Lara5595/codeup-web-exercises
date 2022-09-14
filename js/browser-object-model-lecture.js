536291ba1a7dd00811ddcb9255b21af9c8d14d9d
David Lara <david.a.lara05@gmail.com>
2022-09-14 13:44:57 -0500

    new function

--- js/browser-object-model-lecture.js
+++ js/browser-object-model-lecture.js
@@ -1,15 +1,28 @@
-let sayHey = function () {
-    // let count makes it start in 0
-    let count = 0;
-    function hey() {
-        if (count <= 10) { // Makes it count to 10 on console
-            console.log('hey' + count);  // + count combines hey and number
-            count++   // makes it go up in number
-        }
-    }
+// let sayHey = function () {
+//     // let count makes it start in 0
+//     let count = 0;
+//     function hey() {
+//         if (count <= 10) { // Makes it count to 10 on console
+//             console.log('hey' + count);  // + count combines hey and number
+//             count++   // makes it go up in number
+//         }
+//     }
 // give interval function runs hey every milisecond which is 1000
-    setInterval(hey, 1000);
+//     setInterval(hey, 1000);
+//
+// } // This function ^^ counts to 10 in 1000 ms but it does not stop counting behind the scenes
 
+
+let count = 0;
+let thisTimeItWillStop;
+function hey() {
+    if (count <= 10) {
+        console.log("hey " + count);
+        count ++
+    } else {
+        clearInterval(thisTimeItWillStop);
+    }
+   thisTimeItWillStop = setInterval(hey, 1000)
 }
 
-
+// This function ^^ will make it stop even in the background
