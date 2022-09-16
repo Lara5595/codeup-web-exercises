159247474da949852d4a236596755687b65fb4f2
David Lara <david.a.lara05@gmail.com>
2022-09-16 10:40:36 -0500

    finished

--- js/dom-events-lecture.js
+++ js/dom-events-lecture.js
@@ -15,41 +15,68 @@ function eventHandler() {
 // We can use addEventListener with two arguments: the type of event and the function that will be triggered.
 
 // theButton.addEventListener("click", eventHandler);  0R
-theButton.onclick = eventHandler;
+// The three steps of handling events
 
-// more advance of writing it is
-// document.querySelector("button").addEventListener('click',function () {
-//     alert('you clicked me!');
+// 1. Get a reference to the element that will trigger the event
+// let theButton = document.querySelector("button");
+
+
+// 2. Write the function that will be triggered by the event
+// A function is a set of instructions.  This kind of function that is triggered by an event is called an event handler, or also called callback
+// The function contains the instructions that will run when the event is triggered
+
+// function eventHandler(){
+//     // alert("You clicked me!");
+//     document.querySelector('p').innerHTML = "You changed me.";
+// }
+
+// 3. Register the handler
+
+// We can use addEventListener with two arguments: the type of event and the function that will be triggered
+
+// theButton.addEventListener('click', eventHandler);
+
+// theButton.onclick = eventHandler;
+
+// document.querySelector("button").addEventListener('click', function(){
+//     alert('You clicked  me!');
 // });
 
+// document.querySelector(".square").addEventListener('click', function(){
+//     alert("I am a square");
+// })
+// and my color is " + window.getComputedStyle(document.querySelector(".square"), null).getPropertyValue('background-color')
 
-// Make the square alert
 let theSquare = document.querySelector('.square');
 
-let theHandler = function () {
-    alert('I am a square ' + 'and my color is ' + window.getComputedStyle(document.querySelector('.square'), null) .getPropertyValue('background-color'));
+let theHandler = function(){
+    // alert("I am a square " + window.getComputedStyle(document.querySelector(".square"), null).getPropertyValue('background-color'));
 }
 
-// theSquare.addEventListener('click', theHandler);
-theSquare.addEventListener('click', function (e) {
-    console.log(e.target.className);
-    console.log(getComputedStyle(e.target).backgroundColor);
+theSquare.addEventListener('mousemove', function(e){
+    // console.log(e.target.className);
+    // let theColor = getComputedStyle(e.target).backgroundColor
+    // console.log(theColor);
+    // document.getElementById("anotherSquare").style.backgroundColor = theColor;
+    alert("Event!");
 });
 
+let theButton = document.querySelector('button');
 
+let buttonClickFunction = function(){
+    document.querySelector('p').innerText = "You changed me";
+}
 
-
-// changes the text by clicking the button
-let changeP = document.getElementById('change-P');
-// let button1 = document.getElementById('btn'); works without this
- let buttonP = function () {
-     changeP.innerText = 'It worked'
- }
- document.getElementById('btn').addEventListener('click',buttonP)
-
-
-
-// document.querySelector('body').addEventListener('mouseout', function (event){
-//     alert('free flashlight! Just sign up for email');
-//     console.log(getComputedStyle(event,))
+document.querySelector('body').addEventListener('resize', function(event){
+    alert("Free Flashlights! Just sign up for email newsletter!");
+    // console.log(getComputedStyle(event.target).backgroundColor);
 })
+
+theButton.onclick = buttonClickFunction;
+
+// Get the user input from a form field
+let formInput = document.getElementById("formInput");
+
+formInput.onkeyup = function(){
+    console.log(formInput.value);
+}
