01002ed4ae0692c12ca7a9f8c0d9791c5d6377a7
David Lara <david.a.lara05@gmail.com>
2022-09-22 10:02:39 -0500

    saved

--- js/dom-traversal.js
+++ js/dom-traversal.js
@@ -19,10 +19,23 @@ $(function (){
     $( "ul li").first().css("font-size", 'larger');
 
     // . last targets the last element in the li and makes it underline dotted
-    $("li a").last().css("text-decoration", "underline dotted");
+    $("li a").last().css("text-decoration", "underline wavy");
 
+    // .children selects all the children of main
+    $("button").on('click', function (){
+       // $("main").children().css("font-family", "sans-serif");
+       //  $("main").toggleClass("sansSerif")
+        $("ul").last().children().children().css("text-decoration", "underline dotted");
+    });
 
-
+        // .next
+    $("h2").css({
+        "color": "blue",
+        "text-decoration" : "underline",
+        "cursor": "pointer"
+    }).click(function (){
+        $(this).next().slideToggle();
+    })
 
 
 
