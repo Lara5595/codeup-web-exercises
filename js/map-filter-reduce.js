ce93b34bcd316809807e17789c0c7fcb662bce7a
David Lara <david.a.lara05@gmail.com>
2022-10-02 12:25:09 -0500

    finished 5

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -235,11 +235,13 @@ let totalYears = users.reduce((acc, element, index, array) => {
 console.log(`The total years of experience is ${totalYears}`);
 
 // 5. Use .reduce to get the longest email from the list of users.
-let longestEmail = users.reduce((acc, element, index, array ) => {
-    console.log(element.email);
-})
-
-
+let longestEmail = users.reduce((acc, element, index) => {
+    console.log(element.email.length);
+    if (acc.length < element.email.length) {
+        return element.email
+    } return acc
+},"")
+console.log(longestEmail);
 // 6. Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
 
 
